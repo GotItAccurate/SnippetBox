afc12e3181ad22a624055078dc6934742a12d301
Adithya <adithyaselvam1102@gmail.com>
2025-03-07 17:55:33 +0530

    Added URL query string functionality to SnippetView handler.

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
@@ -14,7 +16,12 @@ func Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func SnippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from SnippetView."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet ID %d.", id)
 }
 
 func SnippetCreate(w http.ResponseWriter, r *http.Request) {
