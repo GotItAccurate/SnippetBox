e5c88b351565ae50b555ed8be473bdd1283273f6
Adithya <aadhii.yz@gmail.com>
2025-03-08 17:49:21 +0530

    Added a base template.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -13,13 +13,17 @@ func Home(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl.html")
+	files := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/pages/home.tmpl.html",
+	}
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal server error.", 500)
 		return
 	}
-	err = ts.Execute(w, nil)
+	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal server error.", 500)
