452abfe7dc22464a92993572e35d80ab8a536ee2
Adithya <aadhii.yz@gmail.com>
2025-03-08 19:34:11 +0530

    Added few comments.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,22 +8,30 @@ import (
 	"strconv"
 )
 
+// Handler for GET : '/' :
 func Home(w http.ResponseWriter, r *http.Request) {
+	// Check for absolute route :
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
+
+	// Template files :
 	files := []string{
 		"./ui/html/base.tmpl.html",
 		"./ui/html/partials/nav.tmpl.html",
 		"./ui/html/pages/home.tmpl.html",
 	}
+
+	// Parseing the template files :
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal server error.", 500)
 		return
 	}
+
+	// Writing the parsed template to the ResponseWriter :
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Println(err.Error())
@@ -31,20 +39,27 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Handler for GET : '/snippet/view?id=%d' :
 func SnippetView(w http.ResponseWriter, r *http.Request) {
+	// Parseing the query string :
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
 	}
+
+	// Output for the specified ID :
 	fmt.Fprintf(w, "Display a specific snippet ID %d.", id)
 }
 
+// Handler for POST : '/snippet/create'
 func SnippetCreate(w http.ResponseWriter, r *http.Request) {
+	// Check for POST method :
 	if r.Method != "POST" {
 		w.Header().Set("Allow", http.MethodPost)
 		http.Error(w, "Method Not Allowed.", http.StatusMethodNotAllowed)
 		return
 	}
+
 	w.Write([]byte("Hello from SnippetCreate."))
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,16 +6,21 @@ import (
 )
 
 func main() {
+	// Create a new router/mux :
 	mux := http.NewServeMux()
 
+	// Create a file server for the static files :
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 
+	// Route handler for the static files :
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
+	// Route handlers for the rest of the routes :
 	mux.HandleFunc("/", Home)
 	mux.HandleFunc("/snippet/view", SnippetView)
 	mux.HandleFunc("/snippet/create", SnippetCreate)
 
+	// Starting the server :
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
