519062c8537b5d9c799bca53071fd7e1a1fa6164
Adithya <aadhii.yz@gmail.com>
2025-03-26 19:55:39 +0530

    Added logic for Get snippet method in the model.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"net/http"
 	"strconv"
+
+	"github.com/GotItAccurate/SnippetBox/internal/models"
 )
 
 // Handler for GET : '/' :
@@ -45,8 +48,18 @@ func (app *application) SnippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
 	// Output for the specified ID :
-	fmt.Fprintf(w, "Display a specific snippet ID %d.", id)
+	fmt.Fprintf(w, "%+v", snippet)
 }
 
 // Handler for POST : '/snippet/create'
